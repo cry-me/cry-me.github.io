d62fdc5c7dd0885f72efd0b4da22fe07b1650e86
Randall Ma <randall@randallma.com>
2018-03-27 21:24:09 -0700

    update folder structure

---
+++ gatsby-proj/.cache/async-requires.js
@@ -0,0 +1,24 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---src-pages-design-jsx": require("gatsby-module-loader?name=component---src-pages-design-jsx!/Users/randall/Workspace/cry-me.github.io/gatsby-proj/src/pages/design.jsx"),
+  "component---src-pages-illustration-jsx": require("gatsby-module-loader?name=component---src-pages-illustration-jsx!/Users/randall/Workspace/cry-me.github.io/gatsby-proj/src/pages/illustration.jsx"),
+  "component---src-pages-index-jsx": require("gatsby-module-loader?name=component---src-pages-index-jsx!/Users/randall/Workspace/cry-me.github.io/gatsby-proj/src/pages/index.jsx"),
+  "component---src-pages-design-arch-11-a-jsx": require("gatsby-module-loader?name=component---src-pages-design-arch-11-a-jsx!/Users/randall/Workspace/cry-me.github.io/gatsby-proj/src/pages/design/arch11a.jsx")
+}
+
+exports.json = {
+  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/randall/Workspace/cry-me.github.io/gatsby-proj/.cache/json/layout-index.json"),
+  "design.json": require("gatsby-module-loader?name=path---design!/Users/randall/Workspace/cry-me.github.io/gatsby-proj/.cache/json/design.json"),
+  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/randall/Workspace/cry-me.github.io/gatsby-proj/.cache/json/layout-index.json"),
+  "illustration.json": require("gatsby-module-loader?name=path---illustration!/Users/randall/Workspace/cry-me.github.io/gatsby-proj/.cache/json/illustration.json"),
+  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/randall/Workspace/cry-me.github.io/gatsby-proj/.cache/json/layout-index.json"),
+  "index.json": require("gatsby-module-loader?name=path---index!/Users/randall/Workspace/cry-me.github.io/gatsby-proj/.cache/json/index.json"),
+  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/randall/Workspace/cry-me.github.io/gatsby-proj/.cache/json/layout-index.json"),
+  "design-arch-11-a.json": require("gatsby-module-loader?name=path---design-arch-11-a!/Users/randall/Workspace/cry-me.github.io/gatsby-proj/.cache/json/design-arch-11-a.json")
+}
+
+exports.layouts = {
+  "component---src-layouts-index-jsx": require("gatsby-module-loader?name=component---src-layouts-index-jsx!/Users/randall/Workspace/cry-me.github.io/gatsby-proj/.cache/layouts/index.js")
+}
---
+++ gatsby-proj/.cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "/Users/randall/Workspace/cry-me.github.io/gatsby-proj/src/layouts/index.jsx"
+  import data from "/Users/randall/Workspace/cry-me.github.io/gatsby-proj/.cache/json/layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
